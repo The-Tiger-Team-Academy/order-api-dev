69c23d7ed6658225e90a8b339d3e221f4ac95a03
BestTTTA <bst12546@gmail.com>
2024-12-20 11:27:37 +0700

    Redeploy

--- app/routers/lazada/auth_laz.py
+++ app/routers/lazada/auth_laz.py
@@ -12,5 +12,3 @@ def auth_laz():
     app_key = 131467
     link = f"https://auth.lazada.com/oauth/authorize?response_type=code&force_auth=true&redirect_uri={call_back}&client_id={app_key}"
     return {"Auth link": link}
-
-# https://auth.lazada.com/oauth/authorize?response_type=code&force_auth=true&redirect_uri=https://google.com/&client_id=131467
